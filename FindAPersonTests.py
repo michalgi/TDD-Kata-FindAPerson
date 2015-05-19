a1d876b601d7965ad0add43c82402e632757e4b2
michalgi <michalgi1991@gmail.com>
2015-05-19 09:32:59 +0300

    RED-test_if_there_are_map_inconsistencies

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -31,7 +31,12 @@ class FindAPersonTests(unittest.TestCase):
         location_exist = self.crowdmap.if_there_are_map_inconsistencies("Or A.")
         self.assertTrue(location_exist)		
 		
-		
+    def test_if_there_are_map_inconsistencies(self):
+        crowdmap_with_consistencies = Crowdmap(["I met Or A. at AA house Bangkok",
+                                  "We found Or A. R.I.P at AA valley",
+                                  "Missing Cowboy"])
+        location_exist = self.crowdmap.if_there_are_map_inconsistencies("Or A.")
+        self.assertFalse(location_exist)		
 
 			
         
